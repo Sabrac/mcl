398dc74d3dfbbbc764e93d9542cda567a7658a11
Sabrac <Sabrac@users.noreply.github.com>
2021-01-05 11:32:56 +0700

    Initialize cordova/ionic

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+
+import { CoreModule } from '@core';
+import { SharedModule } from '@shared';
+import { AuthModule } from '@app/auth';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { AboutModule } from './about/about.module';
+import { SettingsModule } from './settings/settings.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    IonicModule.forRoot(),
+    CoreModule,
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    SettingsModule,
+    AboutModule,
+    AuthModule,
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
+  ],
+  declarations: [AppComponent],
+  providers: [
+    Keyboard,
+    StatusBar,
+    SplashScreen
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
